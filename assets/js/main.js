9542ae2744101ef54629a3b0b888fdb25483c32f
Roemer Naber <200611@student.glu.nl>
2021-09-14 14:33:51 +0200

    disco lichten toegevoegd

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+console.log("kgviojfk");
+let red = document.getElementById("r");
+let green = document.getElementById("g");
+let blue = document.getElementById("b");
+
+function roodFunction(){
+    if (red.style.opacity==='1'){
+        red.style.opacity = '0';
+    }
+    else{
+        red.style.opacity= '1';
+    }
+}
+function groenFunction(){
+    if (green.style.opacity==='1'){
+        green.style.opacity = '0';
+    }
+    else{
+        green.style.opacity= '1';
+    }
+}
